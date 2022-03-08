2b2905927edebd68d706c9db5d5089bfd33b0463
ChesKnow <Roman.Chesanov@outlook.com>
2022-03-08 11:26:27 +0300

    added HW test file

---
+++ src/main/java/guru/qa/HWTest.java
@@ -0,0 +1,14 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HWTest {
+
+    @Test
+    void assertTrueTest() {
+        assertTrue(3 > 0);
+    }
+
+}
